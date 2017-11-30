fa892ef8247359c055ca98ef25fd5e9e256e2699
Matt <matthewlockewong@gmail.com>
2017-11-29 22:21:35 -0800

    added child page menu

--- page.php
+++ page.php
@@ -8,8 +8,13 @@ if (have_posts()):
     <h2> <?php the_title()?> </h2>
     <p><?php the_content() ?></p>
   </article>
-
-
+  <?php
+  $args = array(
+    'child_of' => get_top_ancestor_id(),
+    'title_li' => ''
+  ) ?>
+  <?php wp_list_pages($args) ?>
+  <?php echo $post->ID ?>
   <?php endwhile;
 
 else :

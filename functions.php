9f975d490f5385c4a515afdf64a3c0c133ba104d
Matt <matthewlockewong@gmail.com>
2017-11-30 16:28:18 -0800

    added blog info, custom page templates

--- functions.php
+++ functions.php
@@ -1,16 +1,37 @@
 <?php
 
 
+function get_top_ancestor_id() {
+  //if there is a parent, return parent's id
+
+  global $post;
+
+  if ( $post->post_parent) {
+    $ancestors = array_reverse(get_post_ancestors($post->ID));
+    return $ancestors[0];
+  }
+  return $post->ID;
+}
+
+// Does page have children?
+function has_children() {
+  global $post;
+
+  $pages = get_pages('child_of=' . $post->ID);
+  return count($pages);
+}
+
 function learningWordpress_resources() {
   wp_enqueue_style('style', get_stylesheet_uri());
 }
 
 add_action('wp_enqueue_scripts', 'learningWordpress_resources');
-
 //Navigation Menus
 register_nav_menus(array(
   "primaryyy" => __( "Primary Menuu"),
   'footer' => __('Footer Menu')
 ))
+
+
 
 ?>
